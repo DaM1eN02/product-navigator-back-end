8a728275ab14c5e8d980952e1b1bc9bc1b71a7e9
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-09-27 18:44:07 +0200

    27.09.2021 18:43 minor
    Visibility changes + Login Page created

--- home.js
+++ home.js
@@ -1,26 +1,23 @@
-function homePage() {
-    window.location.href = "home.html";
-}
-
 var home = document.getElementById('home');
 var products = document.getElementById('products');
 var markets = document.getElementById('markets');
 
+function homePage() {
+    window.location.href = "home.html";
+}
+
 function home() {
-    home.style.visibility = 'visible';
-    products.style.visibility = 'hidden';
-    markets.style.visibility = 'hidden';
     alert('Hey');
 }
 
 function products() {
-    home.style.visibility = 'hidden';
-    products.style.visibility = 'visible';
-    markets.style.visibility = 'hidden';
+    
 }
 
 function markets() {
-    home.style.visibility = 'hidden';
-    products.style.visibility = 'hidden';
-    markets.style.visibility = 'visible';
+    
+}
+
+function login() {
+    window.location.href = "login.html";
 }
