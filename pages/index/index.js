03203933e8151091d4c388a8b9053036374affbc
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-09-29 11:53:24 +0200

    29.09.2021 major
    Major Layout Changes

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,5 @@
-var login = document.getElementById('login');
-var home = document.getElementById('home');
-var products = document.getElementById('products');
-var markets = document.getElementById('markets');
-
-var caption = document.getElementById('caption');
-
 function homePage() {
     window.location.href = "../index/index.html";
-}
-
-function home() {
-    
-}
-
-function products() {
-    
-}
-
-function markets() {
-    
 }
 
 function login() {
