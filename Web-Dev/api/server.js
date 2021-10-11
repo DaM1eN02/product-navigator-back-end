b437c1cb4763455a29e066b13ece9dfb0e3a3ae0
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-10-11 11:21:53 +0200

    Express Install

--- Web-Dev/api/server.js
+++ Web-Dev/api/server.js
@@ -1,3 +1,16 @@
-const http = require('http');
+const { application } = require('express');
+const express = require('express');
 
-const server = http.createServer;
+const server = express();
+
+
+
+//Routes
+
+server.get('/', (req,res) => {
+    res.send('Worked');
+})
+
+// Server listens to the requests
+
+server.listen(3000);
