f58fe8c76aaddf9420e9bff5a5d5967fdc258b74
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-10-13 12:04:01 +0200

    DB Connection to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv/config');
 
 const server = express();
 
@@ -15,3 +17,16 @@ server.get('/', (req,res) => {
 // Server listens to the requests
 
 server.listen(3000);
+
+
+// DB Connection
+
+const uri = process.env.db_connection;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+
+  console.log('DB Connection');
+  
+  client.close();
+});
