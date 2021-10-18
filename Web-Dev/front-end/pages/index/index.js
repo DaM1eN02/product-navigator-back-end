031a4dc46fd3f107a8c4cae909657af0fe91e59b
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-10-18 13:52:46 +0200

    Login SPA Fixed

--- Web-Dev/front-end/pages/index/index.js
+++ Web-Dev/front-end/pages/index/index.js
@@ -1,3 +1,9 @@
 function homePage() {
     window.location.href = "../index/index.html";
 }
+
+function loginPage() {
+    window.location.href = "../login/login.html";
+    document.getElementById("formLogin").style.display = "block";
+    document.getElementById("formRegister").style.display = "none";
+}
--- Web-Dev/front-end/pages/login/login.js
+++ Web-Dev/front-end/pages/login/login.js
@@ -1,9 +1,35 @@
+function loginPage() {
+    document.getElementById("formLogin").style.display = "block";
+    document.getElementById("formRegister").style.display = "none";
+}
+
+function registerPage() {
+    document.getElementById("formLogin").style.display = "none";
+    document.getElementById("formRegister").style.display = "block";
+}
+
 function login() {
-    document.getElementById("formLogin").hidden = false;
-    document.getElementById("formRegister").hidden = true;
+    let username = document.getElementById('LoginUsername').value;
+    let password = document.getElementById('LoginPassword').value;
+    const LoginData = {
+        id = username,
+        pw = password
+    }
 }
 
 function register() {
-    document.getElementById("formLogin").hidden = true;
-    document.getElementById("formRegister").hidden = false;
+    let username = document.getElementById('RegisterUsername').value;
+    let email = document.getElementById('RegisterEmail').value;
+    let password1 = document.getElementById('RegisterPassword1').value;
+    let password2 = document.getElementById('RegisterPassword2').value;
+
+    if (password1 = password2) {
+        const RegisterData = {
+            id = username,
+            email = email,
+            password = password1
+        }
+    } else {
+        alert('Your Passwords do not match each other');
+    }
 }
