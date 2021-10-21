1b9c19f1fda27a92e4902c5aff8ca75e78fdd16c
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-10-21 11:43:12 +0200

    Front End Authentication finished

--- Web-Dev/front-end/pages/index/index.js
+++ Web-Dev/front-end/pages/index/index.js
@@ -4,6 +4,4 @@ function homePage() {
 
 function loginPage() {
     window.location.href = "../login/login.html";
-    document.getElementById('formLogin').style.display = "block";
-    document.getElementById('formRegister').style.display = "none";
 }
--- Web-Dev/front-end/pages/login/login.js
+++ Web-Dev/front-end/pages/login/login.js
@@ -12,9 +12,14 @@ function login() {
     let username = document.getElementById('LoginUsername').value;
     let password = document.getElementById('LoginPassword').value;
     const LoginData = {
-        id = username,
-        pw = password
-    }
+        "id": username,
+        "pw": password
+    };
+
+    // Send a token to the API and see if the password and username fits
+
+    // If true then clear all elements and send back to the main page
+    // If false then clear the password
 }
 
 function register() {
@@ -23,13 +28,20 @@ function register() {
     let password1 = document.getElementById('RegisterPassword1').value;
     let password2 = document.getElementById('RegisterPassword2').value;
 
-    if (password1 = password2) {
+    if (password1 == password2) {
         const RegisterData = {
-            id = username,
-            email = email,
-            password = password1
-        }
+            "id": username,
+            "email": email,
+            "password": password1
+        };
+
+        document.getElementById('RegisterUsername').value = "";
+        document.getElementById('RegisterEmail').value = "";
+        document.getElementById('RegisterPassword1').value = "";
+        document.getElementById('RegisterPassword2').value = "";
     } else {
         alert("Your Passwords do not match each other");
-    }
+        document.getElementById('RegisterPassword1').value = "";
+        document.getElementById('RegisterPassword2').value = "";
+    };
 }
