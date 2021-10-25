02d25c062977195dcd8556c980348b097ef9bbb9
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-10-25 18:39:23 +0200

    Auth

---
+++ auth/auth/index.js
@@ -0,0 +1,26 @@
+const express   = require('express');
+const app       = express();
+const mongoose  = require ('mongoose');
+const dotenv    = require('dotenv');
+
+//Import Routes
+const authRoute = require('./routes/auth');
+const productRoute = require('./routes/products');
+
+dotenv.config();
+
+//Connect to MongoDB
+
+mongoose.connect(process.env.DB_CONNECT, {UseNewUrlParser: true },
+() => console.log ('connected to db!'));
+
+//Middlewares
+app.use(express.json());
+
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+app.use('/api/products', productRoute);
+app.listen(3000, () => console.log('Server Up and running'));
+
+
