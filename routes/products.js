ed62046e9ad2b86eb3641c285bb57dc2a3063504
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2021-11-08 16:02:02 +0100

    alexatest

--- routes/products.js
+++ routes/products.js
@@ -89,15 +89,73 @@ router.post('/searchProduct', async (req, res) => {
 router.post('/searchProductAlexa', async (req, res) => {
     res.contentType('application/json');
     res.type('json');
+    const GetProductInformation_Handler =  {
+        canHandle(handlerInput) {
+            const request = handlerInput.requestEnvelope.request;
+            return request.type === 'IntentRequest' && request.intent.name === 'GetProductInformation' ;
+        },
+        handle(handlerInput) {
+            const request = handlerInput.requestEnvelope.request;
+            const responseBuilder = handlerInput.responseBuilder;
+            let sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    
+            let say = 'Test';
+    
+            let slotStatus = '';
+            let resolvedSlot;
+            let slotValues = getSlotValues(request.intent.slots); 
+            // getSlotValues returns .heardAs, .resolved, and .isValidated for each slot, according to request slot status codes ER_SUCCESS_MATCH, ER_SUCCESS_NO_MATCH, or traditional simple request slot without resolutions
+            
+            const product = null;
+            if(slotValues && slotValues.product){
+                
+                product = product.find({
+                    "name": {
+                        "$regex": req.body.name,
+                        "$options": "i"
+                    }
+                })
+            
+                res.send(product);
+            }
+            
 
-    const product = await Product.find({
-        "name": {
-            "$regex": req.body.name,
-            "$options": "i"
-        }
-    })
-
-    res.send(product);
+            
+            
+            // console.log('***** slotValues: ' +  JSON.stringify(slotValues, null, 2));
+            //   SLOT: product 
+            if (slotValues.product.heardAs) {
+                slotStatus += ' slot product was heard as ' + slotValues.product.heardAs + '. ';
+            } else {
+                slotStatus += 'slot product is empty. ';
+            }
+            if (slotValues.product.ERstatus === 'ER_SUCCESS_MATCH') {
+                slotStatus += 'a valid ';
+                if(slotValues.product.resolved !== slotValues.product.heardAs) {
+                    slotStatus += 'synonym for ' + slotValues.product.resolved + '. '; 
+                    } else {
+                    slotStatus += 'match. '
+                } // else {
+                    //
+            }
+            if (slotValues.product.ERstatus === 'ER_SUCCESS_NO_MATCH') {
+                slotStatus += 'which did not match any slot value. ';
+                console.log('***** consider adding "' + slotValues.product.heardAs + '" to the custom slot type used by slot product! '); 
+            }
+    
+            if( (slotValues.product.ERstatus === 'ER_SUCCESS_NO_MATCH') ||  (!slotValues.product.heardAs) ) {
+                slotStatus += 'A few valid values are, ' + sayArray(getExampleSlotValues('GetProductInformation','product'), 'or');
+            }
+    
+            say += slotStatus;
+    
+    
+            return responseBuilder
+                .speak(say)
+                .reprompt('try again, ' + say)
+                .getResponse();
+        },
+    }
 });
 
 
